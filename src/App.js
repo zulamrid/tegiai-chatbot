c1cf41023579c85dafeb31ccc04f0a14166c6f8c
zul-adn <zbojetx@gmail.com>
2021-09-11 11:30:43 +0700

    add textbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BiMicrophone } from "react-icons/bi";
+import { FaPlay, FaMicrophone } from "react-icons/fa";
 
 
 
@@ -18,12 +18,12 @@ function App() {
     recog.start()
   }
 
-  recog.onstart = function(){
+  recog.onstart = function () {
     console.log("Voice is activated")
     setIsTalk(true)
   }
 
-  recog.onresult = function(event){
+  recog.onresult = function (event) {
 
     const current = event.resultIndex;
     const message = event.results[current][0].transcript
@@ -46,9 +46,23 @@ function App() {
 
   return (
     <div className="container">
-      <button className="btn" onClick={start}  disabled = {isTalk} ><BiMicrophone size={60} color="white" /></button>
+      {/* <button className="btn" onClick={start}  disabled = {isTalk} ><BiMicrophone size={60} color="white" /></button>
       <h3 style={{ color:'#636e72' }}> {isTalk ? "Talking..." : "Click button above to talk"} </h3>
-      <h2>{words}</h2>
+      <h2>{words}</h2> */}
+      <div className="chat-box">
+        akaka
+      </div>
+      <div className="text-box">
+        <div className="rec-container">
+          <FaMicrophone size={25} color="red" onClick={start} />
+        </div>
+        <div className="input-container">
+          <input type="text" placeholder="Input your message here" />
+        </div>
+        <div className="button-container">
+          <button><FaPlay size={15} color="white" /></button>
+        </div>
+      </div>
     </div>
   );
 }
